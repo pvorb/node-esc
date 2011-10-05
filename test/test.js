305dd04ae96fe77a55bf438617bc4777c5e90026
Paul Vorbach <paul@vorb.de>
2011-10-05 18:02:41 +0200

    Move test into subdir

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var esc = require('./'),
+var esc = require('../'),
     fs = require('fs');
 
 var html = '<html><head><meta charset="utf-8"><title>example</title></head><body id="top"><a href="/some/other/file.html">Link</a><a href="http://example.com"><img src="/some/image.png"></a><a href="another-document">another doc</a><a href="#top">top</a></body></html>';
@@ -9,4 +9,4 @@ fs.writeFileSync('test-2.html', esc(html, { 'uri': 'http://example.com/' }));
 
 fs.writeFileSync('test-3.html', esc(html, { 'uri': 'http://example.com/dir/doc.html' }));
 
-esc(undefined);
+esc();
